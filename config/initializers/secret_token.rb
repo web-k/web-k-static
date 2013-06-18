d3b1cedec22c62a4cf262a395e4298133dd843fa
Toshiharu Takematsu <info@newfish.jp>
2013-06-19 03:55:55 +0900

    SimpleWebRTCサンプル追加

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebKStatic::Application.config.secret_token = '847663ae9b91b4f5be0d70cb5fedc15a1232ecec5f2f27bcb22ed380ea503494f083ce5b22e1a882926206234e252b43d88e3d710d8a609e4bd6f30f261f2d7e'
